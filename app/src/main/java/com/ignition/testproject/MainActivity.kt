72b512eff0559f513a51b12b0d261d147c20e1b4
Goncharoff <nik.ignition.work@gmail.com>
2018-09-20 19:11:03 +0300

    init commit

---
+++ app/src/main/java/com/ignition/testproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ignition.testproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ignition.testproject.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
