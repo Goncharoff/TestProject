8edd1dfce9fd9097bd1099c7ab4b1af76663fa8e
Goncharoff <nik.ignition.work@gmail.com>
2018-09-23 12:30:11 +0300

    main list added, added toolbar to places list

--- app/src/main/java/com/ignition/testproject/presentation/listOfPlaces/PlacesListFragment.kt
+++ app/src/main/java/com/ignition/testproject/presentation/listOfPlaces/PlacesListFragment.kt
@@ -2,6 +2,7 @@ package com.ignition.testproject.presentation.listOfPlaces
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,12 +13,15 @@ import com.ignition.testproject.presentation.base.BaseFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_places_list.*
 import javax.inject.Inject
 
 class PlacesListFragment : BaseFragment() {
 
     @Inject
     lateinit var viewModelFactory: DaggerVMFactory
+
+    var placesRvAdapter = PlacesGroupsRVAdapter(mutableListOf())
 
     private val viewModel by lazy { ViewModelProviders.of(this, viewModelFactory).get(PlacesListVM::class.java) }
 
@@ -33,12 +37,24 @@ class PlacesListFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        places_fragment_toolbar.title = getString(R.string.places_title)
+        rv_places_group.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = placesRvAdapter
+        }
+
+        loadPlaces()
+    }
+
+    fun loadPlaces() {
         subscription = viewModel.getPlaces()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ it -> })
+                .subscribe({ it ->
+                    placesRvAdapter.updateData(it.placeUnions)
+                    rv_places_group.adapter = placesRvAdapter
+                })
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
